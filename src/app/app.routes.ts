0891e2dc5218b4e85a4b0b5a1dc5b0fb903af7eb
iury.castro <iury.castro@techlead.com.br>
2025-03-30 20:47:17 -0300

    feat: Initial config to access the remote app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { loadRemoteModule } from '@angular-architects/native-federation';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'remote-app',
+    loadComponent: () =>
+      loadRemoteModule('remote-native-federation', './Component').then(
+        (m) => m.AppComponent
+      ),
+  },
+];
